4a62c578204f9347fb02e81992321b95c2cd0b2e
wxhami <d18102002@mail.ru>
2022-11-25 22:12:33 +0300

    add StringExtensions

---
+++ CensorNew/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace CensorNew;
+
+public static class StringExtensions
+{
+    public static string ToUpperFirstChar(this string input)
+    {
+        return input switch
+        {
+            null => throw new ArgumentNullException(nameof(input)),
+            "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
+            _ => string.Concat(input[0].ToString().ToUpper(), input.AsSpan(1))
+        };
+    }
+}
