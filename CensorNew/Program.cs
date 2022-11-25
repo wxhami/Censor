6f3fd3359739cf7fb2fa3a7d8f5d67869aaddacc
wxhami <d18102002@mail.ru>
2022-11-25 22:18:07 +0300

    Censor Working

--- CensorNew/Program.cs
+++ CensorNew/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using CensorNew;
 
-Console.WriteLine("Hello, World!");
+const string censorWord = "уточка";
+
+Console.WriteLine("specify the path to the directory");
+string Path = Console.ReadLine();
+List<string> Words = new List<string>()
+{
+    "бля",
+    "хуй",
+    "хуя",
+    "ебан",
+    "муд",
+    "ебу"
+};
+
+
+CensorWords(@"C:\Users\vladu\Desktop\Новая папка\файл для цензуры.txt");
+
+void CensorWords(string path)
+{
+    Path = path;
+    DirectoryInfo directoryInfo = new DirectoryInfo(path);
+ 
+    string censor = File.ReadAllText(path);
+    
+    foreach (var word  in Words)
+    {
+      censor =  censor.Replace(word.ToLower(), censorWord);
+      censor = censor.Replace(word.ToUpper(), censorWord);
+      censor = censor.Replace(word.ToUpperFirstChar(), censorWord);
+    }
+
+    Console.WriteLine(censor);
+}
+
